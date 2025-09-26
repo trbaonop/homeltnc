b9969c18c375b51335cccf5f822c2f973004957d
trbaonop <161367764+trbaonop@users.noreply.github.com>
2025-09-26 23:22:46 +0700

    Add files via upload

---
+++ Bài1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bài1
+{
+    public partial class delete : Form
+    {
+        public delete()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string thongbao;
+            thongbao = "Tên đăng nhập là: ";
+            thongbao += this.txtUser.Text;
+            thongbao += "\n\rMật khẩu là: ";
+            thongbao += this.txtPass.Text;
+
+            if (this.chkNho.Checked == true)
+            {
+                thongbao += "\n\rBạn có ghi nhớ.";
+            }
+
+            // Hiện hộp thoại với icon và button OK
+            MessageBox.Show(thongbao, "Thông báo",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void txtPass_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.txtUser.Clear();
+            this.txtPass.Clear();
+            this.txtUser.Focus();
+        }
+    }
+}
